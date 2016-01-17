435f3555a224d2241728e1cea90323da67f9db1d
Alexander Brandi <alexander@pbrandi.dk>
2016-01-17 12:11:47 +0100

    Added StackBuilder to implement the super-like functionality

--- client/app.js
+++ client/app.js
@@ -1,36 +1,5 @@
-var _DECK = [];
-var _COUNTER = 0;
+var _STACK;
 Template.hello.onRendered(function() {
-    // Just to ensure that we only process the data from the latests rendering
-    _DECK = [];
-    _COUNTER = 0;
-
-
-    /*** STACK SETUP/CONFIGURATION **/
-
-    // init the stack object
-    var stack = gajus.Swing.Stack({
-        minThrowOutDistance: 750,
-        throwOutConfidence: function(offset, element) {
-            return Math.min(Math.abs(offset) / element.offsetWidth * 2, 1);
-        }
-    });
-
-    // find all the card elements and add the swing effect
-    $('.tinder .card').each(function() {
-        var elm = $(this);
-        var card = stack.createCard(elm[0]);
-        elm.addClass('in-deck');
-
-        // Add the nasty data-sort attribute to perform the hack
-        elm.attr('data-sort', _COUNTER);
-
-        _DECK.push({
-            card: card,
-            like: true
-        });
-        _COUNTER++;
-    });
 
     // hide the stack for radar effect
     $('.tinder.stack').css({
@@ -39,33 +8,6 @@ Template.hello.onRendered(function() {
 
     // disable selection to prevent crazy blue highlights all over the place
     $('.tinder.viewport').disableSelection();
-
-
-    /** STACK EVENT TRIGGERS **/
-
-    // throw the card out of stack event trigger
-    stack.on('throwout', function(e) {
-        $(e.target).removeClass('in-deck');
-
-        // trigger the swipe events using jquery to trigger the template events
-        var like = e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT;
-        $(e.target).trigger(like ? "likeswipe" : "removeswipe", like);
-    });
-
-    // throw the card into the stack event trigger
-    stack.on('throwin', function(e) {
-        $(e.target).addClass('in-deck');
-
-        // Ugly hack to keep track of the correct order when reverting
-        // Otherwise the cards will overlap incorrectly
-        // I might make sense to add the class '.in-deck' but no.. swing is messy
-        var res = $('.tinder .card').sort(function(a, b) {
-            var contentA = $(a).attr('data-sort');
-            var contentB = $(b).attr('data-sort');
-            return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
-        });
-        $('.tinder.stack').html(res);
-    });
 
 
     /** WINDOW EVENT TRIGGERS **/
@@ -79,6 +21,9 @@ Template.hello.onRendered(function() {
     // onload trigger (Meteor.defer ensures to trigger the event when done rendering)
     Meteor.defer(function(event) {
         $('.tinder .card').trigger("onload", event);
+
+        // make the stack/match interaction
+        _STACK = new StackBuilder('.tinder.stack', '.tinder.card');
 
         // show the 'finding full-stacker' loader for some time, before showing stack
         setTimeout(function() {
@@ -100,6 +45,131 @@ Template.hello.onDestroyed(function() {
 });
 
 
+var StackBuilder = function(stack, cards){
+    var _deck = [];
+    var _counter = 0;
+    var $stack = $(stack);
+    var $cards = $(cards);
+
+    var $swing = gajus.Swing.Stack({
+        minThrowOutDistance: 750,
+        throwOutConfidence: function(offset, element) {
+            return Math.min(Math.abs(offset) / element.offsetWidth * 2, 1);
+        }
+    });
+
+    var _self = this;
+
+    var onThrowOut = function(e){
+        if (_counter <= 0)
+            return;
+
+        $(e.target).removeClass('in-deck');
+
+        // trigger the swipe events using jquery to trigger the template events
+        var like = e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT;
+
+        var card = _deck[_counter - 1];
+        card.like = like;
+        _counter--;
+
+        _self.refill();
+
+        $(e.target).trigger("throwout", card);
+    };
+
+    var onThrowIn = function(e){
+        if (_counter >= _deck.length)
+            return;
+
+        _counter++;
+
+        $(e.target).addClass('in-deck');
+        // Ugly hack to keep track of the correct order when reverting
+        // Otherwise the cards will overlap incorrectly
+        // I might make sense to add the class '.in-deck' but no.. swing is messy
+        var res = $cards.sort(function(a, b) {
+            var contentA = $(a).attr('data-sort');
+            var contentB = $(b).attr('data-sort');
+            return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
+        });
+        // replace cards with the sorted cards
+        $stack.html(res);
+
+        $(e.target).trigger("throwin");
+    };
+
+    this.superlike = function(){
+        if (_counter <= 0)
+            return;
+
+        _deck[_counter - 1].superlike = true;
+        _deck[_counter - 1].$card.throwOut(gajus.Swing.Card.DIRECTION_RIGHT, 0);
+    };
+
+    this.like = function(){
+        if (_counter <= 0)
+            return;
+
+        _deck[_counter - 1].$card.throwOut(gajus.Swing.Card.DIRECTION_RIGHT, 0);
+    };
+
+    this.dislike = function(){
+        if (_counter <= 0)
+            return;
+
+        _deck[_counter - 1].$card.throwOut(gajus.Swing.Card.DIRECTION_LEFT, 0);
+    };
+
+    this.revert = function(){
+        if (_counter >= _deck.length)
+            return;
+
+        _deck[_counter].$card.throwIn(_deck[_counter].like ? gajus.Swing.Card.DIRECTION_RIGHT : gajus.Swing.Card.DIRECTION_LEFT, 0);
+    };
+
+    this.refill = function(){
+        // check if no more cards
+        if (_counter == 0) {
+            // wait and check again, just like the MAC algorithm used in 802.11 standards
+            setTimeout(function() {
+                // if still no cards, add every card again
+                if (_counter == 0) {
+                    for (var i = 0; i < _deck.length; i++) {
+                        _deck[i].$card.throwIn(_deck[i].like ? gajus.Swing.Card.DIRECTION_RIGHT : gajus.Swing.Card.DIRECTION_LEFT, 0);
+                    }
+                }
+            }, 3000);
+        }
+    };
+
+    this.init = function(){
+        $cards.each(function() {
+            var elm = $(this);
+            var card = $swing.createCard(elm[0]);
+            elm.addClass('in-deck');
+
+            // Add the nasty data-sort attribute to perform the hack
+            elm.attr('data-sort', _counter);
+            var id = _counter;
+            _deck.push({
+                id: id,
+                $card: card,
+                like: false,
+                superlike: false
+            });
+            _counter++;
+        });
+
+        $swing.on('throwout', onThrowOut);
+        $swing.on('throwin', onThrowIn);
+
+        return _self;
+    };
+
+    return this.init();
+};
+
 
 AppUtils = {
     TallestWidestElement: function(selector, calcWidth){
@@ -117,21 +187,6 @@ AppUtils = {
     },
     WidestElement: function(selector) {
         return AppUtils.TallestWidestElement(selector, true);
-    },
-    RefillStack: function() {
-        // check if no more cards
-        if (_COUNTER == 0) {
-            // wait and check again, just like the MAC algorithm used in 802.11 standards
-            setTimeout(function() {
-                // if still no cards, add every card again
-                if (_COUNTER == 0) {
-                    for (var i = 0; i < _DECK.length; i++) {
-                        _DECK[i].card.throwIn(_DECK[i].like ? gajus.Swing.Card.DIRECTION_RIGHT : gajus.Swing.Card.DIRECTION_LEFT, 0);
-                        _COUNTER++;
-                    }
-                }
-            }, 3000);
-        }
     },
     MoveElement: function(arr, fromIndex, toIndex) {
         // takes one array element x and moves it to y position
@@ -155,7 +210,7 @@ AppUtils = {
         // POKEMON: GOOOO BLAAAZE!!!!
         Blaze.renderWithData(template, params, parentNode);
     },
-    ShowMatch: function(imageid, data) {
+    ShowMatch: function(imageid, data, superlike) {
         AppUtils.OpenModal({
             modal: {
                 template: 'modalItsAMatch',
@@ -176,7 +231,8 @@ AppUtils = {
             },
             data: {
                 info: data,
-                image: imageid
+                image: imageid,
+                superlike: !!superlike
             }
         });
     },
@@ -217,52 +273,28 @@ AppUtils = {
 
 Template.hello.events({
     'click .tinder.actions .button .revert': function(event, template) {
-        if (_COUNTER >= _DECK.length)
-            return;
-
-        _DECK[_COUNTER].card.throwIn(_DECK[_COUNTER].like ? gajus.Swing.Card.DIRECTION_RIGHT : gajus.Swing.Card.DIRECTION_LEFT, 0);
-        _COUNTER++;
+        _STACK.revert();
+    },
+    'click .tinder.actions .button .super-like': function(event, template){
+        _STACK.superlike();
     },
     'click .tinder.actions .button .like': function(event, template) {
-        if (_COUNTER == 0)
-            return;
-
-        _DECK[_COUNTER - 1].card.throwOut(gajus.Swing.Card.DIRECTION_RIGHT, 0);
+        _STACK.like();
     },
-    'likeswipe': function(event, template) {
-        if (_COUNTER == 0)
+    'throwout': function(event, template, card) {
+        if (!card.like)
             return;
-
-        _DECK[_COUNTER - 1].like = true;
 
         var _self = this;
-        var imageId = _COUNTER - 1;
-
         // IT'S DEFINITELY A MATCH!!
         // just wait a little to give a real-life experience
         // ... and to prevent the swing effect to collide with the modal effect (=lagging)
         setTimeout(function() {
-            AppUtils.ShowMatch(imageId, _self);
+            AppUtils.ShowMatch(card.id, _self, card.superlike);
         }, 500);
-
-        _COUNTER--;
-
-        AppUtils.RefillStack();
     },
     'click .tinder.actions .button .remove': function(event, template) {
-        if (_COUNTER == 0)
-            return;
-
-        _DECK[_COUNTER - 1].card.throwOut(gajus.Swing.Card.DIRECTION_LEFT, 0);
-    },
-    'removeswipe': function(event, template) {
-        if (_COUNTER == 0)
-            return;
-
-        _DECK[_COUNTER - 1].like = false;
-        _COUNTER--;
-
-        AppUtils.RefillStack();
+        _STACK.dislike();
     },
     'onresize .tinder .card, onload .tinder .card': function(event, template) {
         // MMM... delicious width/height calculations
